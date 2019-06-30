c4574d4ce6f5403468c6156961123eedb0ceaa14
n8jhj <n8jhj@live.com>
2019-06-30 15:54:47 -0700

    adding short option for help
    Updated test. It passes!

--- arbory/core.py
+++ arbory/core.py
@@ -3,7 +3,10 @@ import click
 from .__version__ import __version__
 
 
-@click.command()
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+@click.command(context_settings=CONTEXT_SETTINGS)
 @click.version_option(version=__version__)
 @click.argument('dirpath', type=click.Path(exists=True, file_okay=False))
 def basic(dirpath):
--- test/test_help.py
+++ test/test_help.py
@@ -7,6 +7,9 @@ from arbory import basic
 
 
 def test_help():
+    """Make sure help is available in both long and short forms."""
     runner = CliRunner()
     result = runner.invoke(basic, ['--help'])
+    assert result.exit_code == 0
+    result = runner.invoke(basic, ['-h'])
     assert result.exit_code == 0
