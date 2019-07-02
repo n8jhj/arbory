fdab311cb992c96d9c85f8d46496f626e01b0966
n8jhj <n8jhj@live.com>
2019-07-01 23:54:12 -0700

    Parametrize test_tree
    Had to explicitly say, "--include-files True" for this first test, since False is the default now.

--- test/test_tree.py
+++ test/test_tree.py
@@ -2,21 +2,25 @@
 """
 
 from click.testing import CliRunner
+import pytest
 
 from arbory import tree
 
 
-def test_tree(fs):
-    # Set up fake directory.
-    fs.create_file('tree/f_tree.txt')
-    fs.create_file('tree/sub1/f_sub1.txt')
-    # Run test.
-    runner = CliRunner()
-    result = runner.invoke(tree, ['tree'])
-    assert result.output == '\n'.join([
+@pytest.mark.parametrize('cmd, args, output', [
+    (tree, ['tree', '--include-files', 'True'], '\n'.join([
         'tree/',
         '    f_tree.txt',
         '    sub1/',
         '        f_sub1.txt',
         '',
-    ])
+    ])),
+])
+def test_tree(fs, cmd, args, output):
+    # Set up fake directory.
+    fs.create_file('tree/f_tree.txt')
+    fs.create_file('tree/sub1/f_sub1.txt')
+    # Run test.
+    runner = CliRunner()
+    result = runner.invoke(cmd, args)
+    assert result.output == output
