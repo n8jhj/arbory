638a3c463d9b7f2a34e56a10e7cdb95c9b131c3e
n8jhj <n8jhj@live.com>
2019-07-08 14:26:07 -0700

    Update test_help.py
    Check that "Usage: " is in output text.

--- test/test_help.py
+++ test/test_help.py
@@ -10,8 +10,8 @@ def test_help():
     """Make sure help is available in both long and short forms."""
     runner = CliRunner()
     result = runner.invoke(tree, ['--help'])
-    assert result.exit_code == 0
+    assert result.exit_code == 0 and 'Usage: ' in result.output
     result = runner.invoke(tree, ['-h'])
-    assert result.exit_code == 0
+    assert result.exit_code == 0 and 'Usage: ' in result.output
     result = runner.invoke(tree, [])
-    assert result.exit_code == 0
+    assert result.exit_code == 0 and 'Usage: ' in result.output
