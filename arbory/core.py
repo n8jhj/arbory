f6df734cbbb70fdcdf0760fd2524b39cc98eeda2
n8jhj <n8jhj@live.com>
2019-07-08 15:38:59 -0700

    Move tree subcommand to separate module
    Separation of concerns! :D
    Using Group.add_command() method, as recommended at https://stackoverflow.com/a/39228156/7232335

--- arbory/core.py
+++ arbory/core.py
@@ -8,6 +8,7 @@ import pathlib
 import click
 
 from .__version__ import __version__
+from .tree import tree
 
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
@@ -23,34 +24,11 @@ def arb(ctx):
     ctx.obj['config'] = config_spec()
 
 
-@arb.command()
-@click.pass_obj
-@click.argument('dirpath', type=click.Path(exists=True, file_okay=False),
-    required=False)
-@click.option('-f', '--include-files', default=True, type=bool,
-    show_default=True)
-def tree(obj, dirpath, include_files):
-    """Make file tree.
-
-    The specified path must be an existing directory.
-    """
-    cfg = obj['config']
-    for root, dirs, files in os.walk(dirpath):
-        level = root.replace(dirpath, '').count(os.sep)
-        indent = ' ' * 4 * level
-        d_str = os.path.basename(root) + '/'
-        fg_col = cfg['dir_color_fg']
-        bg_col = cfg['dir_color_bg']
-        click.echo(indent + click.style(d_str, fg=fg_col, bg=bg_col))
-        if include_files:
-            subindent = ' ' * 4 * (level + 1)
-            for f_str in files:
-                fg_col = cfg['file_color_fg']
-                click.echo(subindent + click.style(f_str, fg=fg_col))
-
-
 def config_spec():
     config = configparser.ConfigParser()
     config.read(pathlib.Path(__file__).parent / 'config.ini')
     selected = config['DEFAULT']['selected']
     return config[selected]
+
+
+arb.add_command(tree)
