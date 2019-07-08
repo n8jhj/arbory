e43fd41a46dd1d5e4637221389afae3e7a25efc3
n8jhj <n8jhj@live.com>
2019-07-08 13:27:12 -0700

    Remove pyfakefs dependency
    Less dependencies = simpler. Plus, this way I can run the same tests by hand that pytest runs!

--- test/test_tree.py
+++ test/test_tree.py
@@ -18,14 +18,6 @@ from arbory import tree
         '        f_sub1.txt',
         '',
     ])),
-    # All defaults for this directory, implicitly.
-    (tree, [], '\n'.join([
-        'tree/',
-        '    f_tree.txt',
-        '    sub1/',
-        '        f_sub1.txt',
-        '',
-    ])),
     # Without files.
     (tree, ['tree', '-f', 'False'], '\n'.join([
         'tree/',
@@ -34,10 +26,6 @@ from arbory import tree
     ])),
 ])
 def test_tree(cmd, args, output):
-    # Check for directory argument.
-    if len(args) == 0 or len(args[0]) == 0 or args[0][0] == '-':
-        os.chdir('tree')
-    # Run test.
     runner = CliRunner()
     result = runner.invoke(cmd, args)
     assert result.output == output
