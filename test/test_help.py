779566fca7ca7fc7c8df5d53779fbf86ada3fb53
n8jhj <n8jhj@live.com>
2019-07-08 15:25:26 -0700

    Fix test_help.py and test_version.py
    As simple as importing the new name!

--- test/test_help.py
+++ test/test_help.py
@@ -3,15 +3,15 @@
 
 from click.testing import CliRunner
 
-from arbory import tree
+from arbory import arb
 
 
 def test_help():
     """Make sure help is available in both long and short forms."""
     runner = CliRunner()
-    result = runner.invoke(tree, ['--help'])
+    result = runner.invoke(arb, ['--help'])
     assert result.exit_code == 0 and 'Usage: ' in result.output
-    result = runner.invoke(tree, ['-h'])
+    result = runner.invoke(arb, ['-h'])
     assert result.exit_code == 0 and 'Usage: ' in result.output
-    result = runner.invoke(tree, [])
+    result = runner.invoke(arb, [])
     assert result.exit_code == 0 and 'Usage: ' in result.output
--- test/test_version.py
+++ test/test_version.py
@@ -3,12 +3,12 @@
 
 from click.testing import CliRunner
 
-from arbory import tree
+from arbory import arb
 from arbory.__version__ import __version__
 
 
 def test_version():
     runner = CliRunner()
-    result = runner.invoke(tree, ['--version'])
+    result = runner.invoke(arb, ['--version'])
     assert result.exit_code == 0
     assert result.output == 'arbory, version {}\n'.format(__version__)
