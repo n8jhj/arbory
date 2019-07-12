5e34e118935e73cf64dcbea08e593e65c364bd83
n8jhj <n8jhj@live.com>
2019-07-12 09:25:07 -0700

    Add --use option
    Can switch between configurations! Test not yet implemented for this.

--- arbory/subcommands/config.py
+++ arbory/subcommands/config.py
@@ -5,8 +5,18 @@ import click
 
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
+@click.option('-u', '--use')
 @click.pass_obj
-def config(obj):
+def config(obj, use):
     """Manipulate arbory configuration."""
     cfg = obj['config']
-    click.echo('Configuration: {}'.format(cfg['DEFAULT']['selected']))
+    if use is None:
+        click.echo('Configuration: {}'.format(cfg['DEFAULT']['selected']))
+        return
+    if use in cfg:
+        cfg['DEFAULT']['selected'] = use
+        with open('arbory\\config.ini', 'w') as cf:
+            cfg.write(cf)
+        click.echo('Configuration selected: {}'.format(use))
+    else:
+        click.echo('{!r} does not exist.'.format(use))
