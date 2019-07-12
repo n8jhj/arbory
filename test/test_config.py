afcc4c0a646ebfbe9a58bddc8e3e55edbdc25165
n8jhj <n8jhj@live.com>
2019-07-12 10:05:47 -0700

    Add test for --use option
    Implemented temporary configuration fixture, so tests don't permanently modify config.ini.

--- test/test_config.py
+++ test/test_config.py
@@ -1,12 +1,34 @@
 """Test configuration use.
 """
 
+import pathlib
+import shutil
+
 from click.testing import CliRunner
+import pytest
 
 from arbory import arb
+
+
+@pytest.fixture
+def temp_config(tmpdir):
+    cfg_perm = pathlib.Path('arbory') / 'config.ini'
+    cfg_temp = pathlib.Path(tmpdir) / 'config.ini'
+    shutil.copyfile(cfg_perm, cfg_temp)
+    yield
+    shutil.copyfile(cfg_temp, cfg_perm)
 
 
 def test_show_config():
     runner = CliRunner()
     result = runner.invoke(arb, ['config'])
     assert result.output == 'Configuration: DEFAULT\n'
+
+
+@pytest.mark.usefixtures('temp_config')
+def test_use_option():
+    runner = CliRunner()
+    result = runner.invoke(arb, ['config', '--use', 'yo'])
+    assert result.output == "'yo' does not exist.\n"
+    result = runner.invoke(arb, ['config', '--use', 'cobalt'])
+    assert result.output == 'Configuration selected: cobalt\n'
